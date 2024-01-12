abbc74c932e10a4642a8e23173a8d97fc3803d98
alek.nill@yandex.ru <alek.nill@yandex.ru>
2024-01-12 09:46:17 -0800

    Simple hello consumer.

--- SimpleRabbitMq.Consumer/Program.cs
+++ SimpleRabbitMq.Consumer/Program.cs
@@ -1,1 +1,32 @@
-﻿Console.WriteLine("Consumer main thread.");
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+Console.WriteLine("Consumer main thread.");
+
+
+var factory = new ConnectionFactory() { HostName = "192.168.194.128" };
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+channel.QueueDeclare(queue: "hello",
+                     durable: false,
+                     exclusive: false,
+                     autoDelete: false,
+                     arguments: null);
+
+Console.WriteLine(" [*] Waiting for messages");
+
+var consumer = new EventingBasicConsumer(channel);
+consumer.Received += (model, ea) =>
+{
+    var message = Encoding.UTF8.GetString(ea.Body.ToArray());
+    Console.WriteLine($" [*] Received \"{message}\"");
+};
+
+channel.BasicConsume(queue: "hello",
+                     autoAck: true,
+                     consumer: consumer);
+
+Console.WriteLine(" [*] Press enter to exit.");
+Console.ReadLine();
