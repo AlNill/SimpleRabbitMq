af742b9123e7668e9d42702fae1698070b4f9032
alek.nill@yandex.ru <alek.nill@yandex.ru>
2024-01-14 13:15:53 -0800

    Publish/subscribe pattern simple example

--- SimpleRabbitMqProducer/Program.cs
+++ SimpleRabbitMqProducer/Program.cs
@@ -7,24 +7,16 @@ var factory = new ConnectionFactory() { HostName = "192.168.194.128" };
 using var connection = factory.CreateConnection();
 using var channel = connection.CreateModel();
 
-channel.QueueDeclare(queue: "task_queue",
-                     durable: true,
-                     exclusive: false,
-                     autoDelete: false,
-                     arguments: null);
+channel.ExchangeDeclare(exchange: "logs", type: ExchangeType.Fanout);
 
 string message = GetMessage(args);
 var body = Encoding.UTF8.GetBytes(message);
 
-var properties = channel.CreateBasicProperties();
-properties.Persistent = true;
+channel.BasicPublish(exchange: "logs", routingKey: string.Empty, null, body);
+Console.WriteLine($"Sent logs - {message}");
 
-channel.BasicPublish(exchange: string.Empty,
-                     routingKey: "task_queue",
-                     basicProperties: properties,
-                     body: body);
-
-Console.WriteLine($"Sent message - {message}");
+Console.WriteLine($"Press [enter] to exit.");
+Console.ReadLine();
 
 static string GetMessage(string[] args)
 {
