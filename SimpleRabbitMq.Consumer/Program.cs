87eab01a0944186d13fc6e6d643482f0d3bea14f
alek.nill@yandex.ru <alek.nill@yandex.ru>
2024-04-27 06:45:33 -0700

    RPC

--- SimpleRabbitMq.Consumer/Program.cs
+++ SimpleRabbitMq.Consumer/Program.cs
@@ -3,44 +3,64 @@ using RabbitMQ.Client;
 using RabbitMQ.Client.Events;
 
 var factory = new ConnectionFactory { HostName = "192.168.194.128" };
-
 using var connection = factory.CreateConnection();
 using var channel = connection.CreateModel();
 
-channel.ExchangeDeclare(exchange: "topic_logs", type: ExchangeType.Topic);
-
-var queueName = channel.QueueDeclare().QueueName;
-
-if (args.Length < 1)
-{
-    Console.Error.WriteLine("Usage: {0} [binding_key...]",
-                            Environment.GetCommandLineArgs()[0]);
-    Console.WriteLine(" Press [enter] to exit.");
-    Console.ReadLine();
-    Environment.ExitCode = 1;
-    return;
-}
-
-foreach (var bindingKey in args)
-{
-    channel.QueueBind(queue: queueName,
-                      exchange: "topic_logs",
-                      routingKey: bindingKey);
-}
-
-Console.WriteLine(" [*] Waiting for messages.");
-
+channel.QueueDeclare(queue: "rpc_queue",
+                     durable: false,
+                     exclusive: false,
+                     autoDelete: false,
+                     arguments: null);
+channel.BasicQos(prefetchSize: 0, prefetchCount: 1, global: false);
 var consumer = new EventingBasicConsumer(channel);
+channel.BasicConsume(queue: "rpc_queue",
+                     autoAck: false,
+                     consumer: consumer);
+Console.WriteLine(" [x] Awaiting RPC requests");
+
 consumer.Received += (model, ea) =>
 {
+    string response = string.Empty;
+
     var body = ea.Body.ToArray();
-    var message = Encoding.UTF8.GetString(body);
-    var routingKey = ea.RoutingKey;
-    Console.WriteLine($" [x] Received '{routingKey}':'{message}'");
+    var props = ea.BasicProperties;
+    var replyProps = channel.CreateBasicProperties();
+    replyProps.CorrelationId = props.CorrelationId;
+
+    try
+    {
+        var message = Encoding.UTF8.GetString(body);
+        int n = int.Parse(message);
+        Console.WriteLine($" [.] Fib({message})");
+        response = Fib(n).ToString();
+    }
+    catch (Exception e)
+    {
+        Console.WriteLine($" [.] {e.Message}");
+        response = string.Empty;
+    }
+    finally
+    {
+        var responseBytes = Encoding.UTF8.GetBytes(response);
+        channel.BasicPublish(exchange: string.Empty,
+                             routingKey: props.ReplyTo,
+                             basicProperties: replyProps,
+                             body: responseBytes);
+        channel.BasicAck(deliveryTag: ea.DeliveryTag, multiple: false);
+    }
 };
-channel.BasicConsume(queue: queueName,
-                     autoAck: true,
-                     consumer: consumer);
 
 Console.WriteLine(" Press [enter] to exit.");
 Console.ReadLine();
+
+// Assumes only valid positive integer input.
+// Don't expect this one to work for big numbers, and it's probably the slowest recursive implementation possible.
+static int Fib(int n)
+{
+    if (n is 0 or 1)
+    {
+        return n;
+    }
+
+    return Fib(n - 1) + Fib(n - 2);
+}
--- SimpleRabbitMqProducer/Program.cs
+++ SimpleRabbitMqProducer/Program.cs
@@ -1,20 +1,84 @@
-﻿using System.Text;
+﻿using System.Collections.Concurrent;
+using System.Text;
 using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
 
-var factory = new ConnectionFactory { HostName = "192.168.194.128" };
+public class RpcClient : IDisposable
+{
+    private const string QUEUE_NAME = "rpc_queue";
 
-using var connection = factory.CreateConnection();
-using var channel = connection.CreateModel();
+    private readonly IConnection connection;
+    private readonly IModel channel;
+    private readonly string replyQueueName;
+    private readonly ConcurrentDictionary<string, TaskCompletionSource<string>> callbackMapper = new();
 
-channel.ExchangeDeclare(exchange: "topic_logs", type: ExchangeType.Topic);
+    public RpcClient()
+    {
+        var factory = new ConnectionFactory { HostName = "192.168.194.128" };
 
-var routingKey = (args.Length > 0) ? args[0] : "anonymous.info";
-var message = (args.Length > 1)
-              ? string.Join(" ", args.Skip(1).ToArray())
-              : "Hello World!";
-var body = Encoding.UTF8.GetBytes(message);
-channel.BasicPublish(exchange: "topic_logs",
-                     routingKey: routingKey,
-                     basicProperties: null,
-                     body: body);
-Console.WriteLine($" [x] Sent '{routingKey}':'{message}'");
+        connection = factory.CreateConnection();
+        channel = connection.CreateModel();
+        // declare a server-named queue
+        replyQueueName = channel.QueueDeclare().QueueName;
+        var consumer = new EventingBasicConsumer(channel);
+        consumer.Received += (model, ea) =>
+        {
+            if (!callbackMapper.TryRemove(ea.BasicProperties.CorrelationId, out var tcs))
+                return;
+            var body = ea.Body.ToArray();
+            var response = Encoding.UTF8.GetString(body);
+            tcs.TrySetResult(response);
+        };
+
+        channel.BasicConsume(consumer: consumer,
+                             queue: replyQueueName,
+                             autoAck: true);
+    }
+
+    public Task<string> CallAsync(string message, CancellationToken cancellationToken = default)
+    {
+        IBasicProperties props = channel.CreateBasicProperties();
+        var correlationId = Guid.NewGuid().ToString();
+        props.CorrelationId = correlationId;
+        props.ReplyTo = replyQueueName;
+        var messageBytes = Encoding.UTF8.GetBytes(message);
+        var tcs = new TaskCompletionSource<string>();
+        callbackMapper.TryAdd(correlationId, tcs);
+
+        channel.BasicPublish(exchange: string.Empty,
+                             routingKey: QUEUE_NAME,
+                             basicProperties: props,
+                             body: messageBytes);
+
+        cancellationToken.Register(() => callbackMapper.TryRemove(correlationId, out _));
+        return tcs.Task;
+    }
+
+    public void Dispose()
+    {
+        // closing a connection will also close all channels on it
+        connection.Close();
+    }
+}
+
+public class Rpc
+{
+    public static async Task Main(string[] args)
+    {
+        Console.WriteLine("RPC Client");
+        string n = args.Length > 0 ? args[0] : "30";
+        await InvokeAsync(n);
+
+        Console.WriteLine(" Press [enter] to exit.");
+        Console.ReadLine();
+    }
+
+    private static async Task InvokeAsync(string n)
+    {
+        using var rpcClient = new RpcClient();
+
+        Console.WriteLine(" [x] Requesting fib({0})", n);
+        var response = await rpcClient.CallAsync(n);
+        Console.WriteLine(" [.] Got '{0}'", response);
+    }
+}
